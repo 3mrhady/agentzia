f67f8b7c4cd34c477a85a17ad3c324106bc4082f
3mrhady <3mrhady@gmail.com>
2019-10-29 13:50:32 +0200

    new

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
   $('.single-item').slick(
-    {
+{
 autoplay : true,
 speed :300
 }
 
   );
+
+
+  $('.animate').scrolla();
+  $('.animate').scrolla('bind');
+
+
 });
